6d8ea091ca1a6431f079b21fcbc7154594cf378d
Kajee0311 <105401827+Kajee0311@users.noreply.github.com>
2022-06-25 08:23:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -715,7 +715,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"**💿 Movie Name :** {search} \n\n **✅ Join : @Tamil_Mvs_OFfl** "
+        cap = f"**💿 Mᴏᴠɪᴇ Nᴀᴍᴇ :** `{search}` \n\n **✅ Join : @Tamil_Mvs_OFfl** "
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
